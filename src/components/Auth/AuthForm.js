fe608461f48c26fd34361adbc7c34f822f15ad2a
huseyinglc <huseyingulcuu@gmail.com>
2021-05-23 13:17:38 +0300

    auth context is added to manage app wide state

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,10 +1,15 @@
-import { useState, useRef } from "react";
+import { useState, useRef,useContext } from "react";
+import AuthContext from "../../store/auth-context";
 
 import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
+
+
+  const authCtx = useContext(AuthContext);
+
 
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
@@ -57,7 +62,7 @@ const AuthForm = () => {
         }
       })
       .then((data) => {
-        console.log(data);
+        authCtx.login(data.idToken);
       })
       .catch((error) => {
         alert(error.message);
